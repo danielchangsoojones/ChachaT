8c15d6a4b36bb35a6ba1b62f947328d7667e97c2
Daniel Jones <messyjones@gmail.com>
2016-05-14 00:54:50 -0400

    created card detail view controller

---
+++ ChachaT/CardDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CardDetailViewController.swift
+//  ChachaT
+//
+//  Created by Daniel Jones on 5/14/16.
+//  Copyright © 2016 Chong500Productions. All rights reserved.
+//
+
+import UIKit
+
+class CardDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
