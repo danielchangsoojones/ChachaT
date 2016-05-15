1c5007088d8510ba01967ff113cda1c15faeefdc
Daniel Jones <messyjones@gmail.com>
2016-05-15 18:41:08 -0400

    created tableview that works

---
+++ ChachaT/QuestionDetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  QuestionDetailViewController.swift
+//  ChachaT
+//
+//  Created by Daniel Jones on 5/15/16.
+//  Copyright © 2016 Chong500Productions. All rights reserved.
+//
+
+import UIKit
+
+class QuestionDetailViewController: UIViewController {
+    
+    @IBOutlet weak var theTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+extension QuestionDetailViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+}
