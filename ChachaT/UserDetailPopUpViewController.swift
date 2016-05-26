eadc49a4d8678dc721de976e782c50c763a510be
Daniel Jones <messyjones@gmail.com>
2016-05-26 14:37:59 -0400

    changed the textViewEditingBegan

--- ChachaT/UserDetailPopUpViewController.swift
+++ ChachaT/UserDetailPopUpViewController.swift
@@ -9,13 +9,21 @@
 import UIKit
 import Parse
 
+//enums
+public enum Fact {
+    case FactOne
+    case FactTwo
+    case FactThree
+}
+
+
 class UserDetailPopUpViewController: PopUpSuperViewController {
     
     var keyboardHeight : CGFloat = 216
     @IBOutlet weak var theDescriptionTextView: UITextView!
     @IBOutlet weak var theSaveButton: UIButton!
     
-    var factNumber: Fact?
+    var factNumber: Fact = .FactOne
     var factDescriptionText: String?
     
     var delegate: PopUpViewControllerDelegate?
@@ -23,15 +31,18 @@ class UserDetailPopUpViewController: PopUpSuperViewController {
     @IBAction func save(sender: AnyObject) {
         theSaveButton.enabled = false
         let currentUser = User.currentUser()
-        currentUser?.factOne = theDescriptionTextView.text
+        switch factNumber{
+        case .FactOne: currentUser?.factOne = theDescriptionTextView.text
+        case .FactTwo: currentUser?.factTwo = theDescriptionTextView.text
+        case .FactThree: currentUser?.factThree = theDescriptionTextView.text
+        }
         theActivitySpinner.hidden = false
         theActivitySpinner.startAnimating()
         currentUser?.saveInBackgroundWithBlock({ (success, error) in
             if success {
-                self.delegate?.passFactDescription(self.theDescriptionTextView.text, fact: self.factNumber!)
+                self.delegate?.passFactDescription(self.theDescriptionTextView.text, fact: self.factNumber)
                 self.dismissViewControllerAnimated(true, completion: nil)
             }
-            
         })
     }
     
@@ -53,18 +64,20 @@ class UserDetailPopUpViewController: PopUpSuperViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension UserDetailPopUpViewController: UITextFieldDelegate {
+    func textViewDidBeginEditing(textView: UITextView) {
+        activeTextField = textView
+        editingBeginsTextView(textView)
     }
-    */
-
+    
+    func textViewDidEndEditing(textView: UITextView) {
+        if textView.tag == 1 {
+            placeHolderText = "A question about you (e.g. what is your favorite quote?). Remember to write it like someone else is asking you..."
+        } else if textView.tag == 2 {
+            placeHolderText = "Your interesting (hopefully not too obscene) answer"
+        }
+        editingEndedTextView(textView, placeHolderText: placeHolderText)
+    }
 }
