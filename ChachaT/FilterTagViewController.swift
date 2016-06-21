43e88326fb18556b7217b8303ee6f1e4b7e5cc3d
Daniel Jones <messyjones@gmail.com>
2016-06-20 23:20:47 -0400

    added in new filter view controller

---
+++ ChachaT/FilterTagViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FilterTagViewController.swift
+//  ChachaT
+//
+//  Created by Daniel Jones on 6/20/16.
+//  Copyright © 2016 Chong500Productions. All rights reserved.
+//
+
+import UIKit
+
+class FilterTagViewController: OverlayAnonymousFlowViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
