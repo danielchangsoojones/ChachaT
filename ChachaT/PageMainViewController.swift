b734c6e1d82e50f02d668d252c2e28c17da121de
danielchangsoojones <danieljones@Daniels-MacBook-Pro-3.local>
2016-07-22 02:48:47 -0400

    made page view controller cleaner

--- ChachaT/PageMainViewController.swift
+++ ChachaT/PageMainViewController.swift
@@ -18,34 +18,26 @@ class PageMainViewController: PagesController {
         let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let backgroundAnimationNavigationController = mainStoryboard.instantiateViewControllerWithIdentifier("BackgroundAnimationNavigationViewController") as! UINavigationController
         let backgroundAnimationRootViewController = backgroundAnimationNavigationController.viewControllers[0] as! BackgroundAnimationViewController
+        backgroundAnimationRootViewController.navigationItem.title = "Chacha"
         backgroundAnimationRootViewController.pageMainViewControllerDelegate = self
         let profileStoryboard = UIStoryboard(name: "Profile", bundle: nil)
         let profileIndexNavigationController = profileStoryboard.instantiateViewControllerWithIdentifier("ProfileNavigationController") as! UINavigationController
         self.add([backgroundAnimationNavigationController, profileIndexNavigationController])
         self.showPageControl = false
         self.automaticallyAdjustsScrollViewInsets = false
-//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Log Out", style: .Plain, target: self, action: #selector(PageMainViewController.logOut))
-//        setChachaNavigationLogo()
-        
-        // Do any additional setup after loading the view.
     }
     
-    func setChachaNavigationLogo() {
-        let logo = UIImage(named: "ChaCha-Logo-White")
+    func setChachaNavigationLogo() -> UIView {
+        let logo = UIImage(named: "logo-Chacha")
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 38, height: 38))
         imageView.image = logo
         imageView.contentMode = .ScaleAspectFit
         let titleView = UIView(frame: CGRectMake(0,0, 150,40))
         imageView.frame = titleView.bounds
         titleView.addSubview(imageView)
-        self.navigationItem.titleView = titleView
+        return titleView
     }
     
-    func logOut() {
-        User.logOut()
-        performSegueWithIdentifier(.LogInPageSegue, sender: self)
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -60,16 +52,5 @@ protocol PageMainViewControllerDelegate {
 extension PageMainViewController: PageMainViewControllerDelegate {
     func moveToPageIndex(index: Int) {
         self.goTo(index)
-    }
-}
-
-extension PageMainViewController: SegueHandlerType {
-    enum SegueIdentifier: String {
-        // THESE CASES WILL ALL MATCH THE IDENTIFIERS YOU CREATED IN THE STORYBOARD
-        case LogInPageSegue
-    }
-    
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        
     }
 }
