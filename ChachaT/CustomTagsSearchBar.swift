3c7978b306b0be1abcc005f87a04f654d4898b49
danielchangsoojones <danieljones@Daniels-MacBook-Pro-3.local>
2016-07-30 04:49:11 -0400

    got totally transparent search bar to work

--- ChachaT/CustomTagsSearchBar.swift
+++ ChachaT/CustomTagsSearchBar.swift
@@ -21,11 +21,7 @@ class CustomTagsSearchBar: UISearchBar {
         preferredBorderColor = borderColor
         preferredBorderWidth = borderWidth
         preferredBorderRadius = borderRadius
-//        translucent = true //making the background of search box translucent
-//        searchBarStyle = .Minimal
         tintColor = UIColor.whiteColor() //makes the cancel button of search bar and keyboard cursor a certain color
-        searchBarStyle = .Minimal
-        translucent = true //makes the background of searh box translucent, if I get rid of this, it turns black
         
         setSearchIcon()
     }
@@ -39,10 +35,35 @@ class CustomTagsSearchBar: UISearchBar {
         setImage(image, forSearchBarIcon: UISearchBarIcon.Search, state: UIControlState.Normal)
     }
     
+    override func drawRect(rect: CGRect) {
+        let searchBarView = subviews[0]
+        // Find the index of the search field in the search bar subviews.
+        if let index = indexOfSearchFieldInSubviews() {
+            // Access the search field
+            let searchField: UITextField = (searchBarView).subviews[index] as! UITextField
+            
+            searchField.layer.cornerRadius = preferredBorderRadius
+            searchField.layer.borderColor = preferredBorderColor
+            searchField.layer.borderWidth = preferredBorderWidth
+            searchField.backgroundColor = UIColor.clearColor()
+        
+        }
+        
+        //Hacky way to do this. Getting the subview and finding the backgroundview and then setting the alpha to invisible
+        //For some reason, I can't figure out how to get totally transparent without this hack
+        //if apple ever changed the UISearchBar, then this code could break.
+        if let index = indexOfBackgroundViewInSubviews() {
+            let searchBarBackgroundView = searchBarView.subviews[index]
+            searchBarBackgroundView.alpha = 0
+        }
+        
+        super.drawRect(rect)
+    }
+    
     //Purpose: returns the index for the textFieldView of the searchBar
     func indexOfSearchFieldInSubviews() -> Int! {
         var index: Int!
-        let searchBarView = subviews[0] 
+        let searchBarView = subviews[0]
         
         for i in 0 ..< searchBarView.subviews.count {
             if searchBarView.subviews[i].isKindOfClass(UITextField) {
@@ -54,19 +75,19 @@ class CustomTagsSearchBar: UISearchBar {
         return index
     }
     
-    override func drawRect(rect: CGRect) {
-        // Find the index of the search field in the search bar subviews.
-        if let index = indexOfSearchFieldInSubviews() {
-            // Access the search field
-            let searchField: UITextField = (subviews[0]).subviews[index] as! UITextField
-            
-            searchField.layer.cornerRadius = preferredBorderRadius
-            searchField.layer.borderColor = preferredBorderColor
-            searchField.layer.borderWidth = preferredBorderWidth
+    //Purpose: returns the index for the textFieldView of the searchBar
+    func indexOfBackgroundViewInSubviews() -> Int! {
+        var index: Int!
+        let searchBarView = subviews[0]
         
+        for i in 0 ..< searchBarView.subviews.count {
+            if searchBarView.subviews[i].isKindOfClass(UIView) {
+                //this should find the background view, but if apple changes UISearchBar class, then this code could break
+                index = i
+                break
+            }
         }
-        
-        super.drawRect(rect)
+        return index
     }
 
 }
