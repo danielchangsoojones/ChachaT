8c3ca67035f83b17065a3b2331b36f1adbeae0a9
Daniel Jones <messyjones@gmail.com>
2016-09-03 08:42:32 -0400

    made the ProfilePhotoView be accessible to create in code or storyboard

--- ChachaT/ProfilePhotoView.swift
+++ ChachaT/ProfilePhotoView.swift
@@ -12,58 +12,28 @@ class ProfilePhotoView: UIView {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var numberLabel: UILabel!
+    // Our custom view from the XIB file. We basically have to have our view on top of a normal view, since it is a nib file.
     @IBOutlet var view: UIView!
+
     
-    // Our custom view from the XIB file. We basically have to have our view on top of a normal view, since it is a nib file. 
+    //Called when the view is created programmatically
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        xibSetup()
+    }
     
-//    //Called when the view is created programmatically
-//    override init(frame: CGRect) {
-//        //setup any properties here
-//        super.init(frame: frame)
-//        
-//        //Setup view from .xib file
-//        xibSetup()
-//    }
-//    
-//    //Called when the view is created via storyboard
-//    required init?(coder aDecoder: NSCoder) {
-//        // setup any properties here
-//        
-//        super.init(coder: aDecoder)
-//        
-//        // Setup view from .xib file
-//        xibSetup()
-//    }
-    
+    //Called when the view is created via storyboard
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        
+        xibSetup()
+    }
+    
+    //In storyboard we make sure the File Owner, NOT THE VIEW CLASS TYPE, is set to type ProfilePhotoView. If that is not happening, then it creates a recursion loop that crashes the application. Talk to Daniel Jones if this doesn't make sense.
+    func xibSetup() {
         NSBundle.mainBundle().loadNibNamed("ProfilePhotoView", owner: self, options: nil)[0] as! UIView
+        //basically just setting the customView I built on top of a normal view. It's weird, but that's how you load a xib via storyboard
         self.addSubview(view)
         view.frame = self.bounds
     }
     
-//    func xibSetup() {
-//        view = loadViewFromNib()
-//        
-//        // use bounds not frame or it'll be offset
-//        view.frame = bounds
-//        
-//        // Make the view stretch with containing view
-//        view.autoresizingMask = [UIViewAutoresizing.FlexibleWidth, UIViewAutoresizing.FlexibleHeight]
-//        // Adding custom subview on top of our view (over any custom drawing > see note below)
-//        addSubview(view)
-//    }
-//    
-//    func loadViewFromNib() -> ProfilePhotoView {
-//        let bundle = NSBundle(forClass: self.dynamicType)
-//        let nib = UINib(nibName: "ProfilePhotoView", bundle: bundle)
-//        let view = nib.instantiateWithOwner(nil, options: nil)[0] as! ProfilePhotoView
-//        
-//        return view
-//    }
-    
-    
-    
-
 }
