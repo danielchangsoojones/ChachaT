03c1c078388f4f82b9a639db38ffd79f812e0ece
Daniel Jones <messyjones@gmail.com>
2016-09-13 17:59:34 -0400

    the settings page works now, but not segue-able from anywhere yet.

---
+++ ChachaT/SettingsTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SettingsTableViewController.swift
+//  ChachaT
+//
+//  Created by Daniel Jones on 9/13/16.
+//  Copyright © 2016 Chong500Productions. All rights reserved.
+//
+
+import UIKit
+import EFTools
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+extension SettingsTableViewController: SegueHandlerType {
+    enum SegueIdentifier: String {
+        // THESE CASES WILL ALL MATCH THE IDENTIFIERS YOU CREATED IN THE STORYBOARD
+        case OnboardingPageSegue
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        switch segueIdentifierForSegue(segue) {
+        case .OnboardingPageSegue:
+            User.logOut()
+        }
+    }
+}
