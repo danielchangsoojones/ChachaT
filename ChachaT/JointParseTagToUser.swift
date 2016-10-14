5122663db83dbc9555bd4ac3f6db610fa7647ad8
Daniel Jones <messyjones@gmail.com>
2016-10-14 12:17:59 -0400

    started creating joinParseTagToUser

---
+++ ChachaT/JointParseTagToUser.swift
@@ -0,0 +1,19 @@
+//
+//  JointParseTagToUser.swift
+//  ChachaT
+//
+//  Created by Daniel Jones on 10/13/16.
+//  Copyright © 2016 Chong500Productions. All rights reserved.
+//
+
+import Foundation
+
+class JointParseTagToUser {
+    class func parseClassName() -> String {
+        return "JointParseTagToUser"
+    }
+    
+    //TODO: make enums to hold these
+    @NSManaged var tagTitle: String // we will use this as an index to query on.
+    @NSManaged
+}
