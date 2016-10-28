f4f772458915731f8f41d28f4929bf482278accc
Daniel Jones <messyjones@gmail.com>
2016-10-28 13:06:00 -0400

    got rid of the stuff I was just using to transfer over data..

--- ChachaT/Tags.swift
+++ ChachaT/Tags.swift
@@ -9,6 +9,7 @@
 import Foundation
 import Parse
 
+//Keeping this model for now because I (Daniel Jones) am kind of dumb. When we got a 100 users, the production app was still using this data model. So, now I have to wait for the users to come back into the app to transfer their tags to the new data model. So, until that happens, we need to keep this data model in the app ;(
 class Tags: PFObject, PFSubclassing {
     class func parseClassName() -> String {
         return "Tags"
