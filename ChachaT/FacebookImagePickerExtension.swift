a11c1dab68d366fd28c9c9708e7c41d9c40f2393
Daniel Jones <messyjones@gmail.com>
2016-11-08 11:20:38 -0500

    the Facebook picker now goes automatically to Facebook picker.

--- ChachaT/FacebookImagePickerExtension.swift
+++ ChachaT/FacebookImagePickerExtension.swift
@@ -9,11 +9,25 @@
 import Foundation
 import GBHFacebookImagePicker
 import SCLAlertView
+import ParseFacebookUtilsV4
 
 extension EditProfileViewController: GBHFacebookImagePickerDelegate {
     func showFacebookImagePicker() {
         let picker = GBHFacebookImagePicker()
-        picker.presentFacebookAlbumImagePicker(from: self, delegate: self)
+        
+        if let fbAccessToken = FBSDKAccessToken.current(), fbAccessToken.permissions.isEmpty {
+            FBSDKAccessToken.refreshCurrentAccessToken({ (_, _, error) in
+                if error == nil {
+                    //we must refresh the access token in order to see the permissions, then in the GBHFacebookImagePicker, it checks whether we have the permission, otherwise it would re-login the user.
+                    picker.presentFacebookAlbumImagePicker(from: self, delegate: self)
+                } else if let error = error {
+                    _ = SCLAlertView().showInfo("Facebook Error", subTitle: error.localizedDescription)
+                }
+            })
+        } else {
+            //the user is not logged in via Facebook, or the permissions are already available for use, in which case we can just present the picker like normally where an email user would be asked to login w/ Facebook, and the Facebook user would just go right ahead. Somehow, if the user is logged in via email, it still goes to the facebook website for the last logged in user on facebook. Like Safari is caching this user, which is nice because that means even if they signed in via email to the app, they could still access their facebook photos quickly, assuming they had logged in on safari
+            picker.presentFacebookAlbumImagePicker(from: self, delegate: self)
+        }
     }
     
     func facebookImagePicker(imagePicker: UIViewController, didSelectImage image: UIImage?, WithUrl url: String) {
