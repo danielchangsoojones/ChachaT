54b49c074ac0208dcbe70d2a2acf772f3d99566c
Benjamin Jones <jonesbp@indiana.edu>
2016-11-16 17:34:17 -0500

    resized stuff

--- ChachaT/PhotoEditingMasterLayoutView.swift
+++ ChachaT/PhotoEditingMasterLayoutView.swift
@@ -39,16 +39,58 @@ class PhotoEditingMasterLayoutView: UIView {
         setLayout()
     }
     
+    //Making units to work off of. The major units will be the height and width of one of the smaller pictures. The minor unit will be half of one spacing between each picture. The reason I choose half is so that the spacing along the edge of the master view will be smaller than the space between two pictures.
+    
     func setLayout() {
-        let largePhotoEditingView = createPhotoEditingView(CGRect(x: 0, y: 0, w: PhotoEditingViewConstants.mainPhotoEditingViewSideDimension, h: PhotoEditingViewConstants.mainPhotoEditingViewSideDimension)) //the main photo that is surrounded by the siding
+        
+        //One can adjust the spacing and the widths and Heights should resize accordingly to fill the frame of the PhotoEditingMasterLayoutView.
+        let unitSpacing: CGFloat = 0.05 //Change THIS to get bigger or smaller spacings
+        
+        let spacing: CGFloat = unitSpacing * self.frame.width
+        let unitWidth: CGFloat = ((1.0-2*unitSpacing)/3.0) * self.frame.width
+        let unitHeight: CGFloat = ((1.0-2*unitSpacing)/3.0) * self.frame.height
+        
+        //Set heights, widths, x's and y's
+        var widthArray = [unitWidth * 2.0 + spacing]
+        for _ in 1...5 {
+            widthArray.append(unitWidth)
+        }
+        
+        var heightArray = [unitHeight * 2.0 + spacing]
+        for _ in 1...5 {
+            heightArray.append(unitHeight)
+        }
+        
+        var xArray: [CGFloat] = [0.0]
+        for _ in 1...3 {
+            xArray.append(2.0*unitWidth + 2.0*spacing)
+        }
+        xArray += [unitWidth + spacing, 0.0]
+        
+        var yArray: [CGFloat] = [0.0, 0.0, unitHeight + spacing]
+        for _ in 3...5 {
+            yArray.append(2.0*unitHeight + 2.0*spacing)
+        }
+        
+        //Create views
+        for i in 0...5 {
+            let frame = CGRect(x: xArray[i], y: yArray[i], w: widthArray[i], h: heightArray[i])
+            let view = PhotoEditingView(frame: frame, number: i+1)
+            view.tag = i + 1
+            self.addSubview(view)
+        }
+        
+        /*let largePhotoEditingView = createPhotoEditingView(CGRect(x: 0, y: 0, w: PhotoEditingViewConstants.mainPhotoEditingViewSideDimension, h: PhotoEditingViewConstants.mainPhotoEditingViewSideDimension)) //the main photo that is surrounded by the siding
         let sidingStackViews = createSidingStackViews()
         let innerHorizontalStackView = createStackView(.horizontal, distribution: .fillProportionally, views: [largePhotoEditingView, sidingStackViews.verticalSiding])
         masterStackView = createStackView(.vertical, distribution: .fillProportionally, views: [innerHorizontalStackView, sidingStackViews.horizontalSiding])
         self.addSubview(masterStackView)
         masterStackView.snp.makeConstraints { (make) in
             make.edges.equalTo(self)
-        }
+        }*/
     }
+    
+    
     
     //a sidingStackView is one of the rows, surrounding the large PhotoEditingView, on the outside of the square that holds the mini PhotoEditingViews.
     func createSidingStackViews() -> (verticalSiding: UIStackView, horizontalSiding: UIStackView) {
@@ -104,21 +146,22 @@ class PhotoEditingMasterLayoutView: UIView {
     }
     
     func setNewImage(_ image: UIImage, photoNumber: Int) {
-        let photoEditingSubviews = getSubviewsOfView(masterStackView)
+        let photoEditingSubviews = getSubviewsOfView(self)
         for subview in photoEditingSubviews where subview.tag == photoNumber {
             subview.setImage(image: image) //should be only one photoEditingView that has any given tag
         }
     }
     
     func setNewImageFromFile(_ file: AnyObject, photoNumber: Int) {
-        let photoEditingSubviews = getSubviewsOfView(masterStackView)
+        let photoEditingSubviews = getSubviewsOfView(self)
         for subview in photoEditingSubviews where subview.tag == photoNumber {
             subview.setImage(file: file)
         }
     }
     
     func deleteImage(photoNumber: Int) {
-        let photoEditingSubviews = getSubviewsOfView(masterStackView)
+        
+        let photoEditingSubviews = getSubviewsOfView(self)
         for subview in photoEditingSubviews where subview.tag == photoNumber {
             subview.deleteImage() //should be only one photoEditingView that has any given tag
         }
@@ -129,11 +172,7 @@ class PhotoEditingMasterLayoutView: UIView {
         var photoEditingSubviewArray : [PhotoEditingView] = []
         
         for subview in view.subviews {
-            photoEditingSubviewArray = photoEditingSubviewArray + getSubviewsOfView(subview)
-            
-            if let subview = subview as? PhotoEditingView {
-                photoEditingSubviewArray.append(subview)
-            }
+            photoEditingSubviewArray.append(subview as! PhotoEditingView)
         }
         
         return photoEditingSubviewArray
