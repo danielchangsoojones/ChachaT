a211fc727136eda02830bb22e56c0236800396d7
Daniel Jones <messyjones@gmail.com>
2016-11-16 15:48:36 -0500

    something broke when showing the messaging

---
+++ ChachaT/NewCardMessageDataStore.swift
@@ -0,0 +1,9 @@
+//
+//  NewCardMessageDataStore.swift
+//  ChachaT
+//
+//  Created by Daniel Jones on 11/16/16.
+//  Copyright © 2016 Chong500Productions. All rights reserved.
+//
+
+import Foundation
